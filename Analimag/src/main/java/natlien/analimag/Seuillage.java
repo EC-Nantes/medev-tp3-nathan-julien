d08a36d95da6620b898f5031fe33e48761c31805
Julien Daudin <Julien.Daudin@eleves.ec-nantes.fr>
2025-11-24 17:21:39 +0100

    La fonction seuillage n'attends désormais que GestionFichier pour fonctionnner

--- Analimag/src/main/java/natlien/analimag/Seuillage.java
+++ Analimag/src/main/java/natlien/analimag/Seuillage.java
@@ -4,6 +4,7 @@
  */
 package natlien.analimag;
 
+import java.util.List;
 import java.util.ArrayList;
 
 /**
@@ -18,7 +19,22 @@ public class Seuillage {
      * @param D
      * @return
      */
-    public int seuillage(){
-        return 1;
+    public int seuillage(GestionFichier img,int D){
+        ArrayList<List<Integer>> hache=new ArrayList<List<Integer>>();
+        int longueur=img.hauteur;
+        int largeur=img.largeur;
+        ArrayList<ArrayList<Integer>> HD=img.data;
+        for(ArrayList<Integer> Ligne : HD){
+            ArrayList<Integer> hacher=new ArrayList<Integer>();
+            for(int pixel : Ligne){
+                if(pixel>D){
+                    hacher.add(1);
+                }
+                else{
+                    hacher.add(0);
+                }
+            }
+            hache.add(hacher);
+        }
     }
 }
